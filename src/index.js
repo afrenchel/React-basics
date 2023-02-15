08bc8e86cdab530367dd532d59a64a79990d45e1
Alina Frenchel <alina.frenchel@gmail.com>
2023-02-15 16:21:36 +0200

    Create booklist-CSS with React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css"; //CSS
 
 //Component (General convention is to call this APP)
 //Nesting components
 function Booklist() {
   return (
-    <section>
+    <section className="booklist">
       <Book />
     </section>
   );
@@ -13,21 +14,33 @@ function Booklist() {
 
 const Book = () => {
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/713lRguJLsL._AC_UL600_SR600,400_.jpg"
+        alt=""
+      ></img>
+      <h1>You're my little cuddle bug</h1>
+      <h4>Nicola Edwards </h4>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/713lRguJLsL._AC_UL600_SR600,400_.jpg"
-    alt=""
-  ></img>
-);
-const Title = () => <h1>You're my little cuddle bug</h1>;
-const Author = () => <h4>Nicola Edwards</h4>;
+
+//*Separate components with inline CSS
+// const Image = () => (
+//   <img
+//     src="https://images-na.ssl-images-amazon.com/images/I/713lRguJLsL._AC_UL600_SR600,400_.jpg"
+//     alt=""
+//   ></img>
+// );
+// const Title = () => <h1>You're my little cuddle bug</h1>;
+// const Author = () => (
+//   <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+//     Nicola Edwards
+//   </h4>
+// );
+
+/////////////////////////////////////////////////////
+
 // const Person = () => <h2>this is Alina and this is my first component</h2>;
 
 // const Message = () => {
