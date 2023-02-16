4415e67ca759989e31131385691d5fb7200a026f
Alina Frenchel <alina.frenchel@gmail.com>
2023-02-16 16:07:29 +0200

    Mapping booklist-Destructuring-Props

--- src/index.js
+++ src/index.js
@@ -2,25 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css"; //CSS
 
+//*Setting variables for booklist
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/713lRguJLsL._AC_UL600_SR600,400_.jpg",
+    title: "You're my little cuddle bug",
+    author: "Nicola Edwards",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/61UW7b6Lv+L._AC_UL900_SR900,600_.jpg",
+    title: "Why women deserve LESS",
+    author: "Myron Gaines",
+  },
+];
+
 //Component (General convention is to call this APP)
 //Nesting components
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = () => {
+//?Book component
+
+const Book = (props) => {
+  const { img, title, author } = props; //destructuring the props object
+  //attribute, eventHandler
+  const clickHandler = () => {
+    alert("hello world");
+  };
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/713lRguJLsL._AC_UL600_SR600,400_.jpg"
-        alt=""
-      ></img>
-      <h1>You're my little cuddle bug</h1>
-      <h4>Nicola Edwards </h4>
+      <img src={img} alt=""></img>
+      <h1>{title}</h1>
+      <h4>{author} </h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
     </article>
   );
 };
